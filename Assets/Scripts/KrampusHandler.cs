c07fb99a407c8e9e662447bd3fdf4a8e2c67b93d
Hans <52242687+colossusqw@users.noreply.github.com>
2025-10-19 15:09:06 -0300

    yay

--- Assets/Scripts/KrampusHandler.cs
+++ Assets/Scripts/KrampusHandler.cs
@@ -10,21 +10,25 @@ public class KrampusHandler : MonoBehaviour
 
     [Header("Declaração de audio")]
     [SerializeField] private AudioSource AS;
+    [SerializeField] private AudioSource ASS;
     [SerializeField] private AudioClip Slowstep;
     [SerializeField] private AudioClip Faststep;
     [SerializeField] private AudioClip Scream1;
 
     [Header("Controle de comportamento")]
+    public bool temporary = true;
     public bool attacking = false;
+    public bool follower = false;
     public bool startle = false;
     public bool currentlyStartled = false;
-    private bool screamed = false;
+    public bool screamed = false;
 
     [Header("Variáveis de comportamento")]
     [SerializeField] private float stalkSpeed = 3f;
     [SerializeField] private float leapSpeed = 8f;
     [SerializeField] private float leapDist = 8f;
     [SerializeField] public float startleRange = 8f;
+    [SerializeField] public float lifeTime = 25f;
 
     void FixedUpdate()
     {
@@ -33,6 +37,11 @@ public class KrampusHandler : MonoBehaviour
             AnimatorMove.SetBool("Stalking", false);
             AnimatorMove.SetBool("Running", false);
             return;
+        }
+        if(temporary)
+        {
+            temporary = false;
+            Invoke("Removeself", lifeTime);
         }
 
         HandlePlayerChase();
@@ -77,15 +86,14 @@ public class KrampusHandler : MonoBehaviour
             AnimatorMove.SetBool("Scared", true);
             if (!screamed)
             {
-                if (!AS.isPlaying) AS.PlayOneShot(Scream1);
+                ASS.PlayOneShot(Scream1);
                 screamed = true;
             }
-            if (!AS.isPlaying) AS.PlayOneShot(Slowstep);
+            if (!AS.isPlaying) AS.PlayOneShot(Slowstep, 0.5f);
             RB.linearVelocity = -transform.forward*stalkSpeed + new Vector3(0f, -1f, 0f);
         }
         else
         {
-            screamed = false;
             if (playerDirection.magnitude > 20f)
             {
                 AnimatorMove.SetBool("Running", false);
@@ -96,13 +104,25 @@ public class KrampusHandler : MonoBehaviour
             {
                 AnimatorMove.SetBool("Running", false);
                 AnimatorMove.SetBool("Stalking", true);
-                if (!AS.isPlaying) AS.PlayOneShot(Slowstep);
+                if (!AS.isPlaying) AS.PlayOneShot(Slowstep, 0.5f);
                 RB.linearVelocity = transform.forward*stalkSpeed + new Vector3(0f, -1f, 0f);
+            }
+            else if (follower)
+            {
+                AnimatorMove.SetBool("Running", false);
+                AnimatorMove.SetBool("Stalking", false);
+
+                RB.linearVelocity = transform.forward*0 + new Vector3(0f, -0.1f, 0f);
             }
             else
             {
                 AnimatorMove.SetBool("Running", true);
                 AnimatorMove.SetBool("Stalking", false);
+                if (!screamed)
+                {
+                    ASS.PlayOneShot(Scream1);
+                    screamed = true;
+                }
                 if (!AS.isPlaying) AS.PlayOneShot(Faststep);
                 RB.linearVelocity = transform.forward*leapSpeed + new Vector3(0f, -3f, 0f);
             }
